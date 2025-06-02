afb18fc9c1ea91e2e383c6550a26f5e7b56d224f
blaqsimi42 <onisimiloluwa793@gmail.com>
2025-06-02 15:37:53 -0700

    changes

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,25 +1,24 @@
+import React from "react";
 import clsx from "clsx";
 
-type BoundedProps = {
-  as?: React.ElementType;
+type BoundedProps<T extends React.ElementType> = {
+  as?: T;
   className?: string;
-  children: React.ReactNode;
-};
+  children?: React.ReactNode;
+} & React.ComponentPropsWithoutRef<T>;
 
-export const Bounded = ({
-  as: Comp = "section",
+export function Bounded<T extends React.ElementType = "section">({
+  as,
   className,
   children,
   ...restProps
-}: BoundedProps) => {
+}: BoundedProps<T>) {
+  const Comp = as || "section";
   return (
-    <Comp
-      className={clsx("px-4 first:pt-10 md:px-6", className)}
-      {...restProps}
-    >
+    <Comp className={clsx("px-4 first:pt-10 md:px-6", className)} {...restProps}>
       <div className="mx-auto flex w-full max-w-7xl flex-col items-center">
         {children}
       </div>
     </Comp>
   );
-};
+}
