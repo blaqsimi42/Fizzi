8cd711070653e72d72ff8fc0c467521511745096
blaqsimi42 <onisimiloluwa793@gmail.com>
2025-06-02 17:20:26 -0700

    changed

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,31 +1,25 @@
-import React from "react";
 import clsx from "clsx";
 
-type AsProp<T extends React.ElementType> = {
-  as?: T;
+type BoundedProps = {
+  as?: React.ElementType;
+  className?: string;
+  children: React.ReactNode;
 };
 
-type PropsToOmit<T extends React.ElementType, P> = keyof (AsProp<T> & P);
-
-type BoundedProps<T extends React.ElementType> = {
-  className?: string;
-  children?: React.ReactNode;
-} & AsProp<T> &
-  Omit<React.ComponentPropsWithoutRef<T>, PropsToOmit<T, { className?: string; children?: React.ReactNode }>>;
-
-export function Bounded<T extends React.ElementType = "section">({
-  as,
+export const Bounded = ({
+  as: Comp = "section",
   className,
   children,
   ...restProps
-}: BoundedProps<T>) {
-  const Comp = as || "section";
+}: BoundedProps) => {
   return (
-    <Comp className={clsx("px-4 first:pt-10 md:px-6", className)} {...restProps}>
+    <Comp
+      className={clsx("px-4 first:pt-10 md:px-6", className)}
+      {...restProps}
+    >
       <div className="mx-auto flex w-full max-w-7xl flex-col items-center">
         {children}
       </div>
     </Comp>
   );
-}
-
+};
